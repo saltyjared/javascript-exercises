c7b2ef06ef626cf11e60c733ca3f7daa8df15eea
Jared Guevara <jaredpaulgue@berkeley.edu>
2024-10-07 14:58:55 -0700

    Finish JS exercise 2: repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // handle edge case of num being a negative number
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    res = '';
+    for (let i = 0; i < num; i++) {
+        res += string;
+    }
+    return res
 };
 
 // Do not edit below this line
