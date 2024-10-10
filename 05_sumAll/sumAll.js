026744f07d136e274b231145009f15e431f09623
saltyjared <jaredpaulgue@gmail.com>
2024-10-09 21:38:21 -0700

    Finish exercise 5: sum all numbers between 2 integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a < 0) || (b < 0)){
+        return 'ERROR';
+    } else if ((!Number.isInteger(a)) || (!Number.isInteger(b))) {
+        return 'ERROR';
+    }
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
 
+    console.log(`a: ${a}\nb: ${b}`);
+    let sum = 0;
+    for (a; a < b + 1; a++) {
+        sum = sum + a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
