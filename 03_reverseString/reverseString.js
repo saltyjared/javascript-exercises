62a99bc21e34f55b0ae868e36841addbee20132b
saltyjared <jaredpaulgue@gmail.com>
2024-10-09 20:46:27 -0700

    Finish exercise 3: reverse input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const word_array = word.split('');
+    word_array.reverse();
+    return word_array.join('');
 };
 
 // Do not edit below this line
